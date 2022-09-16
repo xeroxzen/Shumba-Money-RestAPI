52f1c5ac2216e1b90695b542a6b89bbffbff8153
Google Jr <andilembele020@gmail.com>
2022-09-16 03:07:02 +0200

    TRANSACTION TYPES, TRANSACTION STATUSES AND unique transaction reference generator

--- RestAPI/transactions/models.py
+++ RestAPI/transactions/models.py
@@ -1,6 +1,34 @@
 from django.db import models
 import uuid
 
+TRANSACTION_STATUSES = (
+    ("pending", "Pending"),
+    ("success", "Success"),
+    ("failed", "Failed"),
+    ("cancelled", "Cancelled"),
+    ('refunded', 'Refunded'),
+    ('reversed', 'Reversed'),
+    ('chargeback', 'Chargeback'),
+    ('disputed', 'Disputed'),
+    ('expired', 'Expired'),
+    ('declined', 'Declined'),
+    ('authorized', 'Authorized'),
+    ('captured', 'Captured'),
+    ('voided', 'Voided'),
+    ('settled', 'Settled'),
+)
+
+TRANSACTION_TYPES = (
+    ("credit", "Credit"),
+    ("debit", "Debit"),
+    ("transfer", "Transfer"),
+)
+
+# function to generate a unique reference number
+def generate_reference():
+    return uuid.uuid4().hex[:6].upper()
+
+
 # Create your models here.
 class Transaction(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -8,9 +36,9 @@ class Transaction(models.Model):
     recipient = models.ForeignKey("recipients.Recipient", on_delete=models.CASCADE, related_name="recipient")
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=20, default="pending")
-    transaction_type = models.CharField(max_length=20, default="transfer")
-    reference = models.CharField(max_length=20, default="")
+    status = models.CharField(max_length=20, default="pending", choices=TRANSACTION_STATUSES)
+    transaction_type = models.CharField(max_length=20, default="transfer", choices=TRANSACTION_TYPES)
+    reference = models.CharField(max_length=20, default=generate_reference, unique=True)
 
     class Meta:
         ordering = ["-created_at"]
