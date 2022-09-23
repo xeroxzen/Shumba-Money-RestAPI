b6f6a1b353a4b8009ff27e80b2b5432c818a459d
xeroxzen <andilembele020@gmail.com>
2022-09-23 12:17:41 -0500

    ... that's the message

--- RestAPI/RestAPI/urls.py
+++ RestAPI/RestAPI/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
 
 
-schema_view = get_swagger_view(title='Shumba Money API')
+schema_view = get_swagger_view(title='Restful API')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
